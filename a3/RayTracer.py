4638a263389080e71cf1de0db910e91b34fd81e4
plmurdoch <pmurdoch@uvic.ca>
2022-12-09 23:39:58 -0800

    progress
    problems to solve:
    No ppm image being printed
    account for the scaling (ellipsoids)
    if overlapping ellipsoids (nearest)
    Lighting rays
    reflection
    refraction
    shadows

--- a3/RayTracer.py
+++ a3/RayTracer.py
@@ -2,46 +2,37 @@ import sys
 import re
 import queue
 import numpy 
-import array
 
 class image_info:
     def __init__(self):
-        near = 0
-        left = 0
-        right = 0
-        bottom = 0
-        top = 0
-        resolution = (0,0)
-        back = (0.0,0.0,0.0)
-        ambient = (0.0,0.0,0.0)
-        output = ""
+        self.near = 0
+        self.left = 0
+        self.right = 0
+        self.bottom = 0
+        self.top = 0
+        self.resolution = (0,0)
+        self.back = (0.0,0.0,0.0)
+        self.ambient = (0.0,0.0,0.0)
+        self.output = ""
 
 class sphere:
     def __init__(self, name, x, y, z, scl_x, scl_y, scl_z, r, g, b, k_a, k_d, k_s, k_r, n):
-        name = name
-        posX = x
-        posY = y
-        posZ = z
-        sclX = scl_x
-        sclY = scl_y
-        sclZ = scl_z
-        red = r
-        green = g
-        KA = k_a
-        KD = k_d
-        KS = k_s
-        KR = k_r
-        N = n
+        self.name = name
+        self.center = numpy.array([x,y,z])
+        self.scale = numpy.array([scl_x,scl_y,scl_z])
+        self.color = numpy.array([r,g,b])
+        self.KA = k_a
+        self.KD = k_d
+        self.KS = k_s
+        self.KR = k_r
+        self.n = n
+
 
 class light:
     def __init__(self, name, x, y, z, l_r, l_g, l_b):
-        name = name
-        posX = x
-        posY = y
-        posZ = z
-        red = l_r
-        green = l_g
-        blue = l_b
+        self.name = name
+        self.position = numpy.array([x,y,z])
+        self.color = numpy.array([l_r,l_g,l_b])
 
 def initializer(input_file):
     spheres = []
@@ -57,22 +48,51 @@ def initializer(input_file):
 
 def scene_builder(info, s_list, l_list):
     scrn = (info.left, info.right, info.top, info.bottom)
+    cam = numpy.array([0,0,1])
     output = open(info.output,"wb")
     initial = "P6\n"+str(info.resolution[0])+" "+str(info.resolution[1])+" 255\n"
+    background = numpy.zeros((info.resolution[1],info.resolution[0],3))
+    eye_ray(background, cam, scrn, info, s_list, l_list)
     output.write(initial.encode())
-    background = array.array('B',[info.back[0]*255,info.back[1]*255,info.back[2]*255]*info.resolution[0]*info.resolution[1])
-    background.tofile(output)
-    for x in range(info.resolution[0]):
-        for y in range(info.resolution[1]):
-            draw(x, y, info, s_list, l_list, output)
+    numpy.save(output, background)
     output.close()
 
 
-def draw(x,y, info, s, l, out):
+def eye_ray(background, cam, scrn, info, s, l):
+    count_y = scrn[2]
+    increment_y = float((scrn[2]-scrn[3])/info.resolution[1])
+    increment_x = float((scrn[0] -scrn[1])/info.resolution[0])
+    true_y = 0
+    while count_y >= scrn[3]: 
+        count_x = scrn[0]
+        true_x = 0
+        while count_x <= scrn[1]:
+            pix_loc = numpy.array([count_x,count_y,0])
+            direction = ((pix_loc - cam)/numpy.linalg.norm((pix_loc - cam)))
+            for s_intersect in s:
+                is_hit = hit_or_miss(s_intersect.center, s_intersect.scale,direction, cam)
+                if is_hit:
+                    background[true_y,true_x] = numpy.array([s_intersect.color[0]*255,s_intersect.color[1]*255,
+                                                             s_intersect.color[2]*255])
+            count_x -= increment_x
+            true_x += 1
+        count_y -= increment_y
+        true_y += 1
     return
     
-    
-    
+    #need hit/miss calculator now
+def hit_or_miss(center, scale, direct, cam_loc):
+    calc_1 = 2 * numpy.dot(direct, cam_loc-center)
+    calc_canon = pow(numpy.linalg.norm(cam_loc - center),2) - 1
+    hit = pow(calc_1,2) - 4*calc_canon
+    if hit > 0:
+        root_1 = (-calc_1 - pow(hit,(1/2)))/2
+        root_2 = (-calc_1 + pow(hit,(1/2)))/2
+        return min(root_1, root_2)
+    elif hit == 0:
+        return (-calc_1)/2
+    else:
+        return        
 def parse_lines(lines, information, spheres, lights):
     buf = []
     if re.search("NEAR", lines):
